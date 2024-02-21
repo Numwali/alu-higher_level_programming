1aed89c58a35cab67c284e4b20d50e43fd3fa236
Noella Umwali <n.umwali@alustudent>
2024-02-21 11:45:31 -0800

    Task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for first_number in range(0,10):
     for second_number in range(first_number +1, 10):
         if first_number == 8 and second_number == 9:
             print("{} {}".format(first_number, send_number))
-        else:print("{}{}, "".format(first_number, second_number), end='')
+        else:print("{}{}, ".format(first_number, second_number), end='')
 
 
