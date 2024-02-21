c0a7db476357aec597ffd3a7e2580cae4bf93015
Noella Umwali <n.umwali@alustudent>
2024-02-21 05:15:05 -0800

    Task0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import random
-number = random.randint (-10, 10)
-if number > 0:
-    print (f"{number:d} is positive")
-elif number == 0: 
-    print (f"{number:d} is zero")
+x = int(input("Please enter an integer: "))
+if x < 0:
+    print('Negative')
+elif x == 0:
+    print('Zero')
 else:
-    print (f"{number:d} is negative")
+    print('Positive')
