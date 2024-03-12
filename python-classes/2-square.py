c879823abb51da6901273661f9c52dcdf69e9906
Numwali <n.umwali@alustudent.com>
2024-03-11 23:53:28 -0700

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Class Square that defines a square"""
+"""Defines a square"""
 
 
 class Square:
-    """Represents a square"""
+    """Represent a square"""
 
-    def __init__(self, size):
-        """Initialize a new Square"""
-        self.__size = size
+    def __init__(self, size=0):
+        """ Method to initialize the square object """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
