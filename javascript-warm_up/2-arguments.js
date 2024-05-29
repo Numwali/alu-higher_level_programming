48594f5ea9747b1f1528061abf69ff7fe11b6ae5
Numwali <numwali@alueducation.com>
2024-05-29 02:41:43 -0700

    Task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(15);
-if (args.length === 1) {
+const args = process.argv.slice(2);
+if (args.length === 0) {
   console.log('No argument');
-} else if (args.length === 5) {
+} else if (args.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
